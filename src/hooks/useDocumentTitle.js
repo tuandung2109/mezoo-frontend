f91efb618d916d87794e5f1ca4d13d2a2ffa1835
tuandung2109 <dinhtuandung10a6@gmail.com>
2025-11-25 10:31:57 +0700

    fix

--- src/hooks/useDocumentTitle.js
+++ src/hooks/useDocumentTitle.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 export const useDocumentTitle = (title) => {
   useEffect(() => {
     const prevTitle = document.title;
-    document.title = title ? `${title} - mezoo` : 'mezoo - Xem Phim Online';
+    document.title = title ? `${title} - Mezoo` : 'Mezoo - Xem Phim Online';
     
     return () => {
       document.title = prevTitle;
